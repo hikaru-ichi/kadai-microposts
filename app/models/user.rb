52253e371f7e4761c7a746b58d0f2401056b0da7
hikaru <thieryhikarin0851@gmail.com>
2021-07-07 11:56:22 +0000

    chap12-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,21 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+
+  has_many :favorites, dependent: :destroy
+  has_many :likes, through: :favorites, source: :micropost 
+
+  def favorite(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+
+  def unfavorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def favorite?(other_micropost)
+    self.likes.include?(other_micropost)
+  end
+
 end
